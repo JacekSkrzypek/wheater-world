0217356e38862b033190195a00cdce2cc0cc4a4f
JacekSkrzypek <jasti05@wp.pl>
2023-04-17 20:03:32 +0200

    Created files and maked the backround of app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './style/main.scss';
+
+function App() {
+  return (
+   <main>
+    <section className='weather'>
+      <article className='weather__article weather__cities'>
+
+      </article>
+      <article className='weather__article weather__cities'>
+
+      </article>
+    </section>
+   </main>
+  );
+}
+
+export default App;
