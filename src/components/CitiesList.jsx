ac6448654a0292bd2334d4cf41c7dbec2dc20e3e
JacekSkrzypek <jasti05@wp.pl>
2023-05-02 20:32:20 +0200

    Added simply style for list of cities

--- src/components/CitiesList.jsx
+++ src/components/CitiesList.jsx
@@ -13,13 +13,23 @@ const CitiesList = () => {
         <article className='weather__article weather__cities'>
             <ul>
                 {citiesList.map((city, i) => {   
-                       const { icon, description } = citiesData[i].weather[0];
-                        return (
-                            <li className='weather__cities__city'>
-                                <p>{city}</p>
-                                <p>{description}</p>
-                            </li>
-                          );
+                        try {
+                            const { main: {temp} } = citiesData[i];
+                            const { icon } = citiesData[i].weather[0];
+                            return (
+                                <li className='weather__cities__city'>
+                                    <p>{city}</p>
+                                    <div className='weather__cities__city__information'>
+                                        <img className='weather__cities__city__information__icon' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+                                        <p>{Math.round(temp - 273.15)}°C</p>
+                                    </div>
+                                </li>
+                              );
+                        }
+                        catch {
+                            console.log("error")
+                        }
+                       
                 })}
             </ul>
         </article>
